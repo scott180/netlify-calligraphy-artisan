d2d02e231d9e45f720124e4d6c2b2baa02f4bcde
xuyongqin <1021151991@qq.com>
2022-03-12 18:06:56 +0800

    1

--- source/index.blade.php
+++ source/index.blade.php
@@ -10,7 +10,7 @@
     <p>人情必有所寄，习惯而成自然。练着练着，书法就成了生活的一部分，记录书法练习轨迹更是有趣有意思的点缀。不能说是点缀，于我而言，练字与记录是相辅相成、缺一不可的关系。</p>
 
     <a href="https://scott180.github.io/calligraphy/" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
+        <img src="https://gitcode.net/xu180/document/-/raw/master/imgs/%E6%B2%BB%E7%96%97%E5%A4%B1%E7%9C%A0%E8%B6%85%E7%BA%A7%E6%9C%89%E6%95%88%E7%9A%84%E5%8A%A8%E5%9B%BE/b3e6bb65gw1ezpt8linb8g20c807pk44.gif" alt="calligraphy" width="223" height="130">
     </a>
 
 
